c4db310b45cad4a9dfc1313988a6a852661e1e01
randolph182 <randolph.estuardo.2012@gmail.com>
2020-07-04 19:07:19 -0600

    Create index.py

---
+++ img_ubuntu_py_jeaguer/index.py
@@ -0,0 +1,65 @@
+
+import asyncio
+import requests
+import json
+import pymongo
+import redis
+import opentelemetry.ext.requests
+from opentelemetry import trace
+from opentelemetry.ext import jaeger
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import ConsoleSpanExporter
+from opentelemetry.sdk.trace.export import SimpleExportSpanProcessor
+from nats.aio.client import Client as NATS
+from nats.aio.errors import ErrConnectionClosed, ErrTimeout, ErrNoServers
+
+
+jaeger_exporter = jaeger.JaegerSpanExporter(
+    service_name="Envio-Datos-BDS", agent_host_name="simplest-agent.default.svc.cluster.local", agent_port=6831
+)
+
+trace.set_tracer_provider(TracerProvider())
+trace.get_tracer_provider().add_span_processor(
+    SimpleExportSpanProcessor(jaeger_exporter)
+)
+
+opentelemetry.ext.requests.RequestsInstrumentor().instrument()
+
+async def run(loop):
+    await nc.connect(servers=["http://35.223.171.148:4222"])
+    future = asyncio.Future()
+
+    async def message_handler(msg):
+        data = json.loads(msg.data.decode())
+        #aca se empizan a enviar los datos a las bases de datos
+        con=pymongo.MongoClient('34.70.196.45',27017)
+        try:
+            db=con.proyecto2
+            db.casos.insert({"name":data["name"],"depto":data["depto"],"age":data["age"],"form":data["form"],"state":data["state"]})
+            print("datos enviados a mongo")
+            with tracer.start_as_current_span("step1"):
+        except Exception as e:
+            print(e)
+            print("problemas con la conexion de mongo")
+        finally:
+            con.close()
+        #print(data)
+        # ======== enviando datos a redis ================
+        try:
+            r = redis.Redis(host='34.70.196.45',port=6379)
+            r.rpush('proyecto2','{"name" : "'+data["name"]+'", "depto" : "'+data["depto"]+'", "age" :'+str(data["age"])+', "form" : "'+data["form"]+'", "state" : "'+data["state"]+'"}')
+            print("datos enviados a redis")
+        except Exception as e:
+            print(e)
+            print("hay problemas en la conexion con redis")
+        print(data)
+    await nc.subscribe("updates", cb=message_handler)
+
+if __name__ == '__main__':
+    nc = NATS()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run(loop))
+    try:
+        loop.run_forever()
+    finally:
+        loop.close()
